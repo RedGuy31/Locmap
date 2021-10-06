13628c2b16d138504c0d39c8157843b2f5866197
RedGuy <I.maisuradze31.19@gmail.com>
2021-10-07 02:01:26 +0400

    DOM2

--- index.js
+++ index.js
@@ -122,6 +122,18 @@ class App {
     inputDistance.focus();
   }
 
+  _hideForm() {
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        "";
+
+    form.style.display = "none";
+    form.classList.add("hidden");
+    setTimeout(() => (form.style.display = "grid"), 1000);
+  }
+
   _toggleElevationField() {
     inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
     inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
@@ -163,14 +175,10 @@ class App {
 
     this.#workouts.push(workout);
 
-    inputDistance.value =
-      inputDuration.value =
-      inputCadence.value =
-      inputElevation.value =
-        "";
-
     this._renderWorkoutMarker(workout);
     this._renderWorkout(workout);
+
+    this._hideForm();
   }
   _renderWorkoutMarker(workout) {
     L.marker(workout.coords)
@@ -184,7 +192,9 @@ class App {
           className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent("workout")
+      .setPopupContent(
+        `${workout.type === "running" ? "🏃‍♂️" : "🚴‍♀️"}${workout.descripton}`
+      )
       .openPopup();
   }
 
